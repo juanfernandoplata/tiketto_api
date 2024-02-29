96342c23d4870b352bfce0266a414b5a8c7f679c
Juan Fernando Plata <juanfernandoplata@gmail.com>
2024-02-29 14:21:34 -0500

    - Idk

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ def handleBusinessAuth( cur, credentials ):
         accessToken = jwt.encode( userData, SECRET_KEY, algorithm = ALGORITHM )
     )
 
-@app.post( "/business/authentication" )
+@app.post( "/business/authenticate" )
 async def businessAuth(
     credentials: AuthRequest
 ) -> AccessToken:
@@ -132,9 +132,9 @@ class EventInfoList( BaseModel ):
 def handleEventsOffering( cur, compId, venueId, eventType ):
     cur.execute(
         f"""
-        select s.*, e.event_date, l.utc_offset
-        from logistics.{ eventType }_shows s, logistics.event e, logistics.venue v, logistics.location l
-        where s.event_id = e.event_id
+        select te.*, e.event_date, l.utc_offset
+        from logistics.{ eventType }_event te, logistics.event e, logistics.venue v, logistics.location l
+        where te.event_id = e.event_id
         and e.venue_id = v.venue_id
         and v.loc_id = l.loc_id
         and e.comp_id = { compId }
@@ -149,7 +149,6 @@ def handleEventsOffering( cur, compId, venueId, eventType ):
     events = []
 
     for event in cur.fetchall():
-        print( event )
         if( event[ -2 ] > utcNow + timedelta( minutes = event[ -1 ] ) ):
             eventCaracts = {}
             for i in range( 1, len( cur.description ) - 2 ):
@@ -195,13 +194,23 @@ def handleEventAvailability( cur, eventId ):
 
     capacity = cur.fetchone()[ 0 ]
 
+    # cur.execute(
+    #     f"""
+    #     select count(*)
+    #     from logistics.event e, logistics.reservation r, logistics.ticket t
+    #     where e.event_id = r.event_id
+    #     and r.reserv_id = t.reserv_id
+    #     and e.event_id = { eventId }
+    #     and r.reserv_state != 'CANCELED'
+    #     """
+    # )
+
     cur.execute(
         f"""
-        select count(*)
-        from logistics.event e, logistics.reservation r, logistics.ticket t
-        where e.event_id = r.event_id
-        and r.reserv_id = t.reserv_id
-        and e.event_id = { eventId }
+        select sum(r.num_tickets)
+        from logistics.reservation r
+        where r.event_id = { eventId }
+        and r.reserv_state != 'CANCELED'
         """
     )
 
@@ -225,6 +234,158 @@ async def eventAvailability(
 
 
 
+class ReservInfo( BaseModel ):
+    eventId: int
+    clientId: str
+    numTickets: int
+
+class ReservId( BaseModel ):
+    reservId: int
+
+# Aqui se hacen validaciones de disponibilidad del evento.
+# Sin embargo, estas verificaciones deberian incorporarse
+# a la base de datos para asegurar la integridad hasta mas
+# bajo nivel.
+def handleEventReserve( cur, reservInfo ):
+    cur.execute(
+        f"""
+        select e.capacity
+        from logistics.event e
+        where e.event_id = { reservInfo.eventId }
+        """
+    )
+
+    capacity = cur.fetchone()[ 0 ]
+
+    cur.execute(
+        f"""
+        select sum(r.num_tickets)
+        from logistics.reservation r
+        where r.event_id = { reservInfo.eventId }
+        and r.reserv_state != 'CANCELED'
+        """
+    )
+
+    reserved = cur.fetchone()[ 0 ]
+
+    if( reservInfo.numTickets > capacity - reserved ):
+        raise HTTPException(
+            status_code = 500,
+            detail = "There are not enough available tickets"
+        )
+    
+    cur.execute(
+        f"""
+        select count(*)
+        from logistics.client c
+        where c.client_id = '{ reservInfo.clientId }'
+        """
+    )
+
+    count = cur.fetchone()[ 0 ]
+
+    if( not count ):
+        cur.execute(
+            f"""
+            insert into logistics.client(client_id)
+            values('{ reservInfo.clientId }')
+            """
+        )
+    
+    cur.execute(
+        f"""
+        insert into logistics.reservation(client_id, event_id, num_tickets, reserv_state)
+        values(
+            '{ reservInfo.clientId }',
+            { reservInfo.eventId },
+            { reservInfo.numTickets },
+            'PENDING'
+        )
+        returning reserv_id
+        """
+    )
+
+    reservId = cur.fetchone()[ 0 ]
+
+    return ReservId( reservId = reservId )
+
+@app.post( "/business/events/{eventId}/reserve" )
+async def eventReserve(
+    reservInfo: ReservInfo,
+
+    user: Annotated[ User, Depends( decodeToken ) ]
+) -> ReservId:
+    with psycopg.connect( **CONN_PARAMS ) as conn:
+        with conn.cursor() as cur:
+            return handleEventReserve( cur, reservInfo )
+
+
+
+
+
+def handleReservationCancel( cur, reservId ):
+    cur.execute(
+        f"""
+        update logistics.reservation
+        set reserv_state = 'CANCELED'
+        where reserv_id = { reservId }
+        """
+    )
+
+    if( not cur.rowcount ):
+        raise HTTPException(
+            status_code = 404,
+            detail = "Resource not found"
+        )
+
+@app.post( "/business/reservations/{reservId}/cancel" )
+async def reservationCancel(
+    reservId: int,
+
+    user: Annotated[ User, Depends( decodeToken ) ]
+):
+    with psycopg.connect( **CONN_PARAMS ) as conn:
+        with conn.cursor() as cur:
+            return handleReservationCancel( cur, reservId )
+
+
+
+
+
+def handleReservationConfirm( cur, reservId ):
+    cur.execute(
+        f"""
+        update logistics.reservation
+        set reserv_state = 'CONFIRMED'
+        where reserv_id = { reservId }
+        """
+    )
+
+    if( not cur.rowcount ):
+        raise HTTPException(
+            status_code = 404,
+            detail = "Resource not found"
+        )
+
+    # Validar que se ejecuto bien el query
+    cur.execute(
+        f"""call create_tickets({ reservId })"""
+    )
+
+@app.post( "/business/reservations/{reservId}/confirm" )
+async def reservationConfirm(
+    reservId: int,
+
+    user: Annotated[ User, Depends( decodeToken ) ]
+):
+    with psycopg.connect( **CONN_PARAMS ) as conn:
+        with conn.cursor() as cur:
+            return handleReservationConfirm( cur, reservId )
+
+
+
+
+
 class TicketInfo( BaseModel ):
     movieName: str
     movieDate: str
@@ -234,9 +395,9 @@ class TicketInfo( BaseModel ):
 def handleTickets( cur, ticketId ):
     cur.execute(
         f"""
-        select ms.movie_name, e.event_date, t.ticket_num, ts.state_type
-        from logistics.movie_shows ms, logistics.reservation r, logistics.event e, logistics.ticket t, logistics.ticket_state ts
-        where ms.event_id = r.event_id
+        select me.movie_name, e.event_date, t.ticket_num, ts.state_type
+        from logistics.movie_event me, logistics.reservation r, logistics.event e, logistics.ticket t, logistics.ticket_state ts
+        where me.event_id = r.event_id
         and r.event_id = e.event_id
         and r.reserv_id = t.reserv_id
         and t.ticket_id = ts.ticket_id
@@ -269,7 +430,7 @@ async def tickets( ticketId: int ) -> TicketInfo:
 
 
 
-def handleAdmitTicket( conn, cur, ticketId ):
+def handleAdmitTicket( cur, ticketId ):
     cur.execute(
         f"""
         update logistics.ticket_state
@@ -284,10 +445,8 @@ def handleAdmitTicket( conn, cur, ticketId ):
             detail = "Resource not found"
         )
 
-    conn.commit()
-
 @app.post( "/tickets/admit/{ticketId}" )
 async def tickets( ticketId: int ):
     with psycopg.connect( **CONN_PARAMS ) as conn:
         with conn.cursor() as cur:
-            handleAdmitTicket( conn, cur, ticketId )
+            handleAdmitTicket( cur, ticketId )
